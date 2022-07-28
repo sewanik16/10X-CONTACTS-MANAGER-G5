dfa8a3d0472cd79a380a5f4cb7b22849a197127d
VickyPalli <palivenkaiah897@gmail.com>
2022-07-28 15:05:05 +0530

    Schema Modified

--- server/models/contacts-model.js
+++ server/models/contacts-model.js
@@ -1,13 +1,40 @@
 const mongoose =  require("mongoose")
-
+const usermodel = require("./user-model")
 const contactSchema = new mongoose.Schema({
-    name:String,
-    designation:String,
-    company: String,
-    industry : String,
-    email: String,
-    phoneNumber: String,
-    country: String
+    contact: [{
+        name: {
+          type: String,
+          required: true,
+        },
+        designation: {
+          type: String,
+          required: true,
+        },
+        company: {
+          type: String,
+          required: true,
+        },
+        industry: {
+          type: String,
+          required: true,
+        },
+        email: {
+          type: String,
+          required: true,
+          unique: true,
+        },
+        phoneNumber: {
+          type: String,
+          required: true,
+          minLength: 10,
+          maxLength:10
+        },
+        country: {
+          type: String,
+          required: true,
+        },
+      }],
+      userId: { type: mongoose.SchemaType.ObjectID, ref: usermodel }
 })
 
 const contactModel = mongoose.model("contacts",contactSchema)
